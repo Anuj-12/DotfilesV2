167c6b890f2584b2c42b58ae27c4f37d5d491c37
Anuj-12 <anujtjs@gmail.com>
2024-12-01 14:33:47 +0530

    Better split panes in wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -15,7 +15,7 @@ config.enable_tab_bar = false
 --config.color_scheme = "Galizur"
 config.color_scheme = "Gooey (Gogh)"
 
-config.window_background_opacity = 0.95
+config.window_background_opacity = 0.75
 
 -- Font
 config.text_background_opacity = 1.0
@@ -26,15 +26,50 @@ config.keys = {
 	-- This will create a new split and run your default program inside it
 	{
 		key = '"',
-		mods = "CTRL|SHIFT",
+		mods = "ALT|SHIFT",
 		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
 	},
 
 	{
 		key = ":",
-		mods = "CTRL|SHIFT",
+		mods = "ALT|SHIFT",
 		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
 	},
+
+	{
+		key = "w",
+		mods = "CTRL",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+	},
+
+	{
+		key = "h",
+		mods = "ALT",
+		action = wezterm.action({ ActivatePaneDirection = "Left" }),
+	},
+
+	{
+		key = "l",
+		mods = "ALT",
+		action = wezterm.action({ ActivatePaneDirection = "Right" }),
+	},
+
+	{
+		key = "k",
+		mods = "ALT",
+		action = wezterm.action({ ActivatePaneDirection = "Up" }),
+	},
+
+	{
+		key = "j",
+		mods = "ALT",
+		action = wezterm.action({ ActivatePaneDirection = "Down" }),
+	},
+}
+
+config.inactive_pane_hsb = { -- Looks of the inactive pane
+	saturation = 0.65,
+	brightness = 0.3,
 }
 
 -- and finally, return the configuration to wezterm
